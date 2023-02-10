9ce0ef35d737158dfae4c2d5b1e9131b5a6eae8b
Salman Khan <salmankhan231999@gmail.com>
2023-02-10 11:35:58 +0530

    add cors configuration

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 mongoose.set("strictQuery", true);
 const todoRouter = require("./routes/todo");
 const connectDB = require("./database/connection");
 require("dotenv").config()
-
 const app = express();
+
+
+const corsOption = {
+    origin: ['http://localhost:3000', 'http://127.0.0.1:5173/'],
+};
+app.use(cors(corsOption));
+
 app.use(express.json());
 app.get('/',(_,res)=> res.send('Hello From Server!'))
 app.use("/todos", todoRouter);
