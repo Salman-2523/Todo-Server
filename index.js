f71fde6cc7619a7b8b7db4bef394904c31affea0
Salman Khan <salmankhan231999@gmail.com>
2023-02-10 11:57:19 +0530

    cors

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ require("dotenv").config()
 const app = express();
 
 
-const corsOption = {
-    origin: ['http://localhost:3000', 'http://127.0.0.1:5173/'],
-};
-app.use(cors(corsOption));
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.get('/',(_,res)=> res.send('Hello From Server!'))
